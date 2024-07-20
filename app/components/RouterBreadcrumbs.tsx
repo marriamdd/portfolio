3d3a7dd67f5332bc279f9fd1a00117215359f56a
marriamdd <davitashvili.m3@gmail.com>
2024-07-21 01:50:02 +0400

    style:add about page markups

--- app/components/RouterBreadcrumbs.tsx
+++ app/components/RouterBreadcrumbs.tsx
@@ -2,23 +2,31 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
-import ListItem, { ListItemProps } from "@mui/material/ListItem";
+import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import Collapse from "@mui/material/Collapse";
 import ListItemText from "@mui/material/ListItemText";
 import Typography from "@mui/material/Typography";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
-import Breadcrumbs from "@mui/material/Breadcrumbs";
+import IconButton from "@mui/material/IconButton";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
+import fileIcon1 from "/public/Vector (5).svg";
+import fileIcon2 from "/public/Vector (4).svg";
+import Image from "next/image";
+import Icon from "@mui/material/Icon";
+import { styled } from "@mui/material/styles";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
-interface ListItemLinkProps extends ListItemProps {
+interface ListItemLinkProps {
   to: string;
   open?: boolean;
 }
 
 const breadcrumbNameMap: { [key: string]: string } = {
   "/inbox": "Inbox",
-  "/inbox/important": "Important",
+  "/inbox/important": "Personal",
   "/trash": "Trash",
   "/spam": "Spam",
   "/drafts": "Drafts",
@@ -44,20 +52,41 @@ function ListItemLink(props: ListItemLinkProps) {
 }
 
 export default function RouterBreadcrumbs() {
-  const [open, setOpen] = React.useState(true);
-  const [selectedOption, setSelectedOption] = React.useState("");
-
-  const handleClick = () => {
-    setOpen((prevOpen) => !prevOpen);
+  const [open, setOpen] = React.useState<string | null>(null);
+  const [selectedOptions, setSelectedOptions] = React.useState<string[]>([
+    "Personal",
+  ]);
+  const [click, setClick] = React.useState<string[]>([]);
+  const handleClick = (prop: string) => {
+    setOpen((prevOpen) => (prevOpen === prop ? null : prop));
   };
 
   const handleOptionClick = (option: string) => {
-    setSelectedOption(option);
+    if (!selectedOptions.includes(option)) {
+      setSelectedOptions((prevSelected) => [option]);
+    }
   };
 
+  const handleInfoClick = (info: string) => {
+    if (!click.includes(info)) {
+      setClick((prevSelected) => [info]);
+    }
+  };
+
+  const handleDelete = (option: string) => {
+    setSelectedOptions((prevSelected) =>
+      prevSelected.filter((item) => item !== option)
+    );
+  };
+  const ArrowIcon = styled(ArrowDownwardIcon)`
+    font-size: 36px;
+    transition: transform 1s ease;
+    transform: ${(props: { rotate: boolean }) =>
+      props.rotate ? "rotate(90deg)" : "rotate(0deg)"};
+  `;
   return (
     <>
-      <div className="pt-[2rem]">
+      <div className="pt-[2rem] animate-fadeIn">
         <h2 className="text-[1.4rem] font-[450] pl-[2rem] ">_about-me</h2>
       </div>
       <Box sx={{ display: "flex", flexDirection: "column", width: 360 }}>
@@ -69,42 +98,146 @@ export default function RouterBreadcrumbs() {
           component="nav"
           aria-label="mailbox folders"
         >
-          <List>
-            <ListItem onClick={handleClick}>
+          <List className="animate-fadeIn ">
+            <ListItem onClick={() => handleClick("info")}>
               <ListItemButton>
-                <ListItemText primary="Info" />
-                {open ? <ExpandLess /> : <ExpandMore />}
+                <ListItemText className="pl-[1rem]" primary="Info" />
+                {open === "info" ? <ExpandLess /> : <ExpandMore />}
               </ListItemButton>
             </ListItem>
-            <Collapse component="li" in={open} timeout="auto" unmountOnExit>
+            <Collapse
+              className="animate-fadeIn"
+              component="li"
+              in={open === "info"}
+              timeout="auto"
+              unmountOnExit
+            >
               <List disablePadding>
-                <ListItem onClick={() => handleOptionClick("Important")}>
+                <ListItem onClick={() => handleOptionClick("Personal")}>
                   <ListItemButton sx={{ pl: 4 }}>
-                    <ListItemText primary="Important" />
+                    <Image
+                      className="pr-[0.7rem]"
+                      src={fileIcon1}
+                      alt="fileIcon"
+                    />
+                    <ListItemText
+                      className={`${
+                        selectedOptions.includes("Personal")
+                          ? "text-[#fff]"
+                          : "text-greyText"
+                      }`}
+                      primary="Personal"
+                    />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem onClick={() => handleOptionClick("Professional")}>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <Image
+                      className="pr-[0.7rem]"
+                      src={fileIcon2}
+                      alt="fileIcon"
+                    />
+                    <ListItemText
+                      className={`${
+                        selectedOptions.includes("Professional")
+                          ? "text-[#fff]"
+                          : "text-greyText"
+                      }`}
+                      primary="Professional"
+                    />
                   </ListItemButton>
                 </ListItem>
               </List>
             </Collapse>
-            <ListItem onClick={() => handleOptionClick("Trash")}>
+            <ListItem onClick={() => handleClick("contact")}>
               <ListItemButton>
-                <ListItemText primary="Trash" />
+                <ListItemText className="pl-[1rem]" primary="Contact" />
+                {open === "contact" ? <ExpandLess /> : <ExpandMore />}
               </ListItemButton>
             </ListItem>
-            <ListItem onClick={() => handleOptionClick("Spam")}>
-              <ListItemButton>
-                <ListItemText primary="Spam" />
-              </ListItemButton>
-            </ListItem>
+            {open === "contact" && (
+              <div className="px-[2rem] py-[1rem] animate-fadeIn flex gap-[1.3rem] flex-col">
+                <div
+                  onClick={() => handleInfoClick("email")}
+                  className="flex gap-[1rem] items-center"
+                >
+                  <EmailIcon />
+                  <h2
+                    className={`${
+                      click.includes("email")
+                        ? "text-[#3f5161] all-transition duration-[1s] ease-out"
+                        : "text-greyText all-transition duration-[1s] ease-out"
+                    }`}
+                  >
+                    davitahvili.m3@gmail.com
+                  </h2>
+                </div>
+                <div
+                  onClick={() => handleInfoClick("number")}
+                  className="flex gap-[1rem] items-center"
+                >
+                  <PhoneIcon />
+                  <h2
+                    className={`${
+                      click.includes("number")
+                        ? "text-[#3f5161] all-transition duration-[1s] ease-out"
+                        : "text-greyText all-transition duration-[1s] ease-out"
+                    }`}
+                  >
+                    {" "}
+                    (+995) 551-10-48-62
+                  </h2>
+                </div>
+              </div>
+            )}
           </List>
         </Box>
-        <Typography className=" pl-[2rem] " variant="body1" sx={{ mt: 2 }}>
-          <span className=" text-[1.6rem] font-[450]">{"// Info  "}</span>
+
+        <Typography className="pl-[2rem]" variant="body1" sx={{ mt: 2 }}>
+          <span className="text-[1.6rem] font-[450]">{"// Info  "}</span>
           <span className="text-greyText font-[450] text-[1.6rem]">
-            {" "}
-            {"/ " + selectedOption}
+            {"/ " + selectedOptions.join(", ")}
           </span>
         </Typography>
       </Box>
+      {selectedOptions.includes("Personal") && (
+        <div className="px-[2rem] py-[2rem]">
+          <p className="text-greyText animate-fadeIn leading-150 text-[1.6rem] font-[450]">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit illo
+            aut amet nesciunt atque blanditiis, quam et id sed tempore pariatur
+            ratione. Quo nihil eaque molestias in, ipsa error accusantium. Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Amet quos
+            reiciendis odio quidem praesentium! Reprehenderit delectus
+            laudantium numquam nihil hic saepe repellat ratione consequuntur,
+            veniam ullam. Quia autem consectetur nihil! Lorem ipsum dolor sit
+            amet consectetur, adipisicing elit. Cupiditate repellat placeat
+            vitae nobis porro dolorum beatae error ducimus repudiandae dicta.
+            Sint quos perferendis accusamus temporibus in quaerat cumque illum!
+            Quo?
+          </p>
+        </div>
+      )}
+      {selectedOptions.includes("Professional") && (
+        <div className="px-[2rem] py-[2rem]">
+          <p className="text-greyText animate-fadeIn leading-150 text-[1.6rem] font-[450]">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit illo
+            aut amet nesciunt atque blanditiis, quam et id sed tempore pariatur
+            ratione. Quo nihil eaque molestias in, ipsa error accusantium. Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Amet quos
+            reiciendis odio quidem praesentium! Reprehenderit delectus
+            laudantium numquam nihil hic saepe repellat ratione consequuntur,
+            veniam ullam. Quia autem consectetur nihil! Lorem ipsum dolor sit
+            amet consectetur, adipisicing elit. Cupiditate repellat placeat
+            vitae nobis porro dolorum beatae error ducimus repudiandae dicta.
+            Sint quos perferendis accusamus temporibus in quaerat cumque illum!
+            Quo?
+          </p>
+        </div>
+      )}
+      <ArrowIcon rotate={selectedOptions.includes("Personal")}>
+        {" "}
+        <ArrowDownwardIcon />
+      </ArrowIcon>
     </>
   );
 }
