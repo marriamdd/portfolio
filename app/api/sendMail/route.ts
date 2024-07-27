da522410e818865f16764b3c3632680952b0adc1
marriamdd <davitashvili.m3@gmail.com>
2024-07-28 01:43:57 +0400

    feat: send gmail functional

---
+++ app/api/sendMail/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, message } = await req.json();
+    console.log("Received:", { name, email, message });
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailForMe = {
+      from: `${name} <${email}>`,
+      to: process.env.EMAIL_USER,
+      subject: `Portfolio Response from ${name}`,
+      text: message,
+    };
+
+    const mailForUser = {
+      from: `${process.env.EMAIL_USER} <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "Thank you for your Response",
+      text: `Hi ${name}, thanks for your Response. I will contact you as soon as I can.`,
+    };
+
+    await transporter.sendMail(mailForMe);
+    await transporter.sendMail(mailForUser);
+
+    console.log("Emails sent successfully");
+
+    return NextResponse.json({ message: "Emails sent successfully" });
+  } catch (error) {
+    console.error("Failed to send email", error);
+    return NextResponse.json(
+      { error: "Failed to send email" },
+      { status: 500 }
+    );
+  }
+}
--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,29 +1,24 @@
 "use client";
 import React from "react";
-import { Button, Form, Input, InputNumber, Layout } from "antd";
+import { Button, Form, Input, Spin, Alert } from "antd";
 import { EemployerInfo } from "../contact/page";
 
-/* eslint-disable no-template-curly-in-string */
 const validateMessages = {
   required: "${label} is required!",
   types: {
     email: "${label} is not a valid email!",
-    number: "${label} is not a valid number!",
-  },
-  number: {
-    range: "${label} must be between ${min} and ${max}",
   },
 };
-/* eslint-enable no-template-curly-in-string */
 
-const onFinish = (values: any) => {
-  console.log(values);
-};
 interface FormCompProps {
   setEmployer: React.Dispatch<React.SetStateAction<EemployerInfo>>;
 }
 
 const FormComp: React.FC<FormCompProps> = ({ setEmployer }) => {
+  const [submit, setSubmit] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [emailError, setEmailError] = React.useState(false);
+
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
@@ -33,9 +28,40 @@ const FormComp: React.FC<FormCompProps> = ({ setEmployer }) => {
       [name]: value,
     }));
   };
+
+  const onFinish = async (values: any) => {
+    setLoading(true);
+    setSubmit(false);
+    setEmailError(false);
+    console.log(values, "values");
+    try {
+      const response = await fetch("/api/sendMail", {
+        method: "POST", // Ensure this is POST
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: values.user.name,
+          email: values.user.email,
+          message: values.user.message,
+        }),
+      });
+
+      if (response.ok) {
+        setSubmit(true);
+      } else {
+        setEmailError(true);
+      }
+    } catch (error) {
+      setEmailError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Form
-      className="xl:border-r-[1px]   animate-fadeIn xl:border-[#1E2D3D]"
+      className="xl:border-r-[1px] animate-fadeIn xl:border-[#1E2D3D]"
       name="nest-messages"
       onFinish={onFinish}
       validateMessages={validateMessages}
@@ -51,20 +77,37 @@ const FormComp: React.FC<FormCompProps> = ({ setEmployer }) => {
       <Form.Item
         name={["user", "email"]}
         label="_email:"
-        rules={[{ type: "email" }]}
+        rules={[{ type: "email", required: true }]}
       >
-        <Input name="gmail" onChange={handleInputChange} />
+        <Input name="email" onChange={handleInputChange} />
       </Form.Item>
 
-      <Form.Item name={["user", "introduction"]} label="_message:">
+      <Form.Item
+        name={["user", "message"]}
+        rules={[{ required: true }]}
+        label="_message:"
+      >
         <Input.TextArea name="message" onChange={handleInputChange} />
       </Form.Item>
       <Form.Item>
-        <Button className="bg-[#1C2B3A]" type="primary" htmlType="submit">
-          submit-message
-        </Button>
+        {loading ? (
+          <Spin />
+        ) : (
+          <>
+            {submit && (
+              <Alert message="Email sent successfully!" type="success" />
+            )}
+            {emailError && (
+              <Alert message="Error sending email." type="error" />
+            )}
+            <Button className="bg-[#1C2B3A]" type="primary" htmlType="submit">
+              submit-message
+            </Button>
+          </>
+        )}
       </Form.Item>
     </Form>
   );
 };
+
 export default FormComp;
